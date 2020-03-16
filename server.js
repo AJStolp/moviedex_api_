1864e79d04b391ba701f290fea92e0f670b9b4cf
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-03-16 12:15:56 -0500

    pushing before lunch - working on token and endpoints

--- server.js
+++ server.js
@@ -7,9 +7,31 @@ const app = express()
 
 app.use(morgan('dev'))
 
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
 
-app.use((req, res) => {
-    console.log('I am working hi!')
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'unAuthorized Request'})
+    }
+    //move to next middleware
+    next()
+})
+
+app.get('/movies', function handleGetMovies(req, res) {
+    res.send('Here are your movies')
+})
+
+app.get('/genre', function handleGenre(req,res) {
+    res.send('here is you genre')
+})
+
+app.get('/country', function handleCountry(req,res){
+    res.send('Here are you movies by country')
+})
+
+app.get('/avg_vote', function handleVote(req, res){
+    req.send('Here are your movies by vote')
 })
 
 PORT = 8000
