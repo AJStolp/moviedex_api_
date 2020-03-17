f8ba212fb42a4560a667a866394187eb2b808342
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-03-17 15:09:18 -0500

    new repo

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next){
+    console.log('validate bearer token middleware')
+    debugger
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-
     if(!authToken || authToken.split(' ')[1]!== apiToken) {
         return res.status(401).json({error: 'unAuthorized Request'})
     }
